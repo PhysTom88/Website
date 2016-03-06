dd0c1bd3cb724b57e3a017757605218872163f76
Tom Ravenscroft <toravenscroft@gmail.com>
2016-03-06 16:54:35 +0000

    home page

--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
-# Create your views here.
+def home(request):
+	return render_to_response(template_name='under_construction.html')
