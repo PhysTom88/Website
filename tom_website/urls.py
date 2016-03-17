05be6546d10562e04fc007c0e13d0847d6165891
Tom Ravenscroft <toravenscroft@gmail.com>
2016-03-17 18:36:01 +0000

    updated css and html

--- tom_website/urls.py
+++ tom_website/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 
-
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('website.urls', namespace='home')) # Main site landing page
