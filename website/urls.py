e1b7365be008f3b0db029905df12d1f997b3043c
Tom Ravenscroft <toravenscroft@gmail.com>
2016-03-18 15:35:05 +0000

    added templates for other site pages

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.home, name='home')
+	url(r'^$', views.home, name='home'),
+	url(r'^about/$', views.about, name='about'),
+	url(r'^blog/$', views.blog, name='blog'),
+	url(r'^photography/$', views.photography, name='photography'),
+	url(r'^trips/$', views.trips, name='trips'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,19 @@ from django.shortcuts import render_to_response
 
 def home(request):
 	return render_to_response(template_name='home.html')
+
+
+def about(request):
+	return render_to_response(template_name='about.html')
+
+
+def blog(request):
+	return render_to_response(template_name='blog.html')
+
+
+def photography(request):
+	return render_to_response(template_name='photography.html')
+
+
+def trips(request):
+	return render_to_response(template_name='trips.html')
