37eafe332970c35fe97e31ea574689b628c0ff32
Tom Ravenscroft <toravenscroft@gmail.com>
2016-04-01 19:17:25 +0100

    can now edit the text and save to db

--- website/urls/main.py
+++ website/urls/main.py
@@ -5,5 +5,5 @@ from website.views import main
 urlpatterns = [
 	url(r'^$', main.home, name='main'),
 	url(r'^about/$', main.about, name='about'),
-	url(r'^edit/', main.edit, name='home_edit'),
+	url(r'^edit/$', main.edit, name='main-edit')
 ]
--- website/views/main.py
+++ website/views/main.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.utils.text import slugify
+from django.shortcuts import render, redirect
+from django.utils import timezone, text
 
 from website.models import TextDescription, BlogPost
 from website.forms import TextDescriptionForm
@@ -7,7 +7,7 @@ from website.forms import TextDescriptionForm
 def home(request):
 	message = TextDescription.objects.get(block_name='welcome')
 	latest_blog = BlogPost.objects.last()
-	latest_blog.slug = slugify(latest_blog.title)
+	latest_blog.slug = text.slugify(latest_blog.title)
 	return render(request, 'main/home.html',
 				  {'message': message, 'blog': latest_blog})
 
@@ -21,6 +21,11 @@ def edit(request):
 	message = TextDescription.objects.get(block_name='welcome')
 	if request.method == 'POST':
 		form = TextDescriptionForm(request.POST, instance=message)
+		if form.is_valid():
+			message = form.save(commit=False)
+			message.mod_date = timezone.now()
+			message.save()
+			return redirect('main:main')
 	else:
 		form = TextDescriptionForm(instance=message)
 	return render(request, 'main/edit.html', {'form': form, 'title': message.block_name})
