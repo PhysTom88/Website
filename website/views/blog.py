5ef6681fac655bb5fbf878d69bcd1f6432091e64
Tom Ravenscroft <toravenscroft@gmail.com>
2016-04-12 13:45:39 +0100

    consolidated views and added links for topics and year filtering

--- website/views/blog.py
+++ website/views/blog.py
@@ -1,5 +1,3 @@
-from datetime import date
-
 from django.shortcuts import render
 from django.views import generic
 
@@ -8,11 +6,20 @@ from website.models import BlogPost
 
 class MainView(generic.View):
 
-	def get(self, request):
-		posts = BlogPost.objects.all().order_by('-published_date')[:6]
+	def get(self, request, identifier=None):
 		topics = map(lambda x: x[1], BlogPost.SUBJECT_CHOICE)
 		years = map(lambda y: y.year,
 				    BlogPost.objects.values_list('published_date')[0])
+
+		if identifier in years:
+			posts = (BlogPost.objects.filter(
+				     published_date=int(identifier)).order_by('-published_date')[:6])
+		elif identifier and identifier.upper() in topics:
+			posts = (BlogPost.objects.filter(
+				     subject=identifier).order_by('-published_date')[:6])
+		else:
+			posts = BlogPost.objects.all().order_by('-published_date')[:6]
+
 		return render(request, 'blog/blog_main.html',
 					  {'posts': posts, 'topics': topics, 'years': years})
 
